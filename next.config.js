ccb311f0a4c5118dae2584351451297cbee3570a
PhatDot1 <patrickloughran121@outlook.com>
2025-06-21 20:38:51 +0100

    fixing shit

--- next.config.js
+++ next.config.js
@@ -1,17 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Remove or comment out the 'output: "export"' line
-  // output: "export", // <-- Remove this line
-  
-  // Keep other configurations you might have
-  experimental: {
-    appDir: true,
-  },
-  
-  // If you have other settings, keep them
-  images: {
-    unoptimized: true, // You might need this if you were using static export
-  },
-}
+  // Disable SWC minification to avoid Terser crashing on worker code
+  swcMinify: false,
 
-module.exports = nextConfig
+  // Remove or comment out the 'output: "export"' line
+  // output: "export", // <-- Removed
+
+  experimental: {
+    // App router is enabled by default in recent Next.js versions,
+    // so you can leave this empty or remove it entirely if unused
+  },
+
+  images: {
+    unoptimized: true, // Useful if you were using static export
+  },
+};
+
+module.exports = nextConfig;
