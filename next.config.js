08b889919f2f3e246e902e658e37588fde1a5397
JamieB2 <118375971+JamieB2@users.noreply.github.com>
2025-06-21 11:04:49 +0100

    Fix Next.js configuration by removing incompatible headers

--- next.config.js
+++ next.config.js
@@ -5,19 +5,6 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Cross-Origin-Opener-Policy',
-            value: 'same-origin-allow-popups',
-          },
-        ],
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
